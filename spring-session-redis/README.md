57ec228bcd195a3af249ed85ae54ed12f9950f03
sandhya <sandhya.bairi154@gmail.com>
2022-09-24 22:29:14 +0530

    adding tested code locally

---
+++ spring-session-redis/README.md
@@ -0,0 +1,4 @@
+#To run app in your system
+Before running the application locally, make sure REDIS server is running.
+
+For windows, Use WSL to install REDIS. 
---
+++ spring-session-redis/src/test/java/org/example/SpringContextLiveTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SessionWebApplication.class)
+public class SpringContextLiveTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+
+    }
+}
